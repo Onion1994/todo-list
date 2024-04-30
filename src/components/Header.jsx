076804bfb5d81333854d507a4f2c0c38cfa7b86c
Renato Cipolla <renato.cipolla94@gmail.com>
2024-04-30 17:46:38 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <header>
+          <h1>Todo List App</h1>
+          {/* Add navigation links here if needed */}
+        </header>
+      );
+}
