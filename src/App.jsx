1e9d05d14b4fc0beb73c0fcfc6783149b387fe41
Renato Cipolla <renato.cipolla94@gmail.com>
2024-05-01 17:59:30 +0100

    add TodoItem and TodoList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList"
 
 export default function App () {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (text) => {
+        const newTodo = {
+            id: Math.random().toString(36).slice(2, 11),
+            text: text
+        }
+        setTodos([...todos, newTodo])
+    }
     return (
         <>
         <Header/>
-        <TodoForm/>
+        <TodoForm addTodo={addTodo}/>
+        <TodoList todos={todos}/>
         </>
     )
 }
